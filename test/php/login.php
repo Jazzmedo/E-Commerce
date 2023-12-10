da2117d2126996874e209c9678eb41346c73b0bc
Jazzmedo <mohamedmelawany@hotmail.com>
2023-12-10 18:32:46 +0200

    test

--- test/php/login.php
+++ test/php/login.php
@@ -7,7 +7,7 @@ session_start();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Sign Up</title>
+  <title>Login</title>
   <link rel="stylesheet" href="../css/styles.css" />
   <script src="https://kit.fontawesome.com/a81368914c.js"></script>
   <link rel="icon" type="image/x-icon" href="../img/Logo.ico">
