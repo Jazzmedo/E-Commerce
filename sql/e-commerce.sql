fb6c6f39f17243187c2db9295f6237ce57c577ad
JazzMedo <mohamedmelawany@hotmail.com>
2024-05-02 04:28:27 +0300

    sql update

--- sql/e-commerce.sql
+++ sql/e-commerce.sql
@@ -1,29 +1,26 @@
-CREATE TABLE Users(
-    u_id int PRIMARY KEY AUTO_INCREMENT,
-    username varchar(200),
-    email varchar(200),
-    passwordd varchar(200)
-    );
+/* Database Name = "shop_db" like in config.php*/
+CREATE TABLE `user_form` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `password` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE products(
-    p_id int PRIMARY KEY,
-    pname varchar(100),
-    price varchar(100),
-    imagee varchar(100)
-    );
+CREATE TABLE `cart` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `user_id` int(100) NOT NULL,
+    `name` varchar(100) NOT NULL,
+    `price` varchar(100) NOT NULL,
+    `image` varchar(100) NOT NULL,
+    `quantity` int(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE carts(
-    id int PRIMARY KEY,
-    cname varchar(100),
-    price varchar(100),
-    cimage varchar(100),
-    quantity int
-    );
-
-CREATE TABLE Student_Course (
-    user_id INT NOT NULL,
-    product_id INT NOT NULL,
-    PRIMARY KEY (user_id, product_id),
-    FOREIGN KEY (user_id) REFERENCES Users(u_id),
-    FOREIGN KEY (product_id) REFERENCES products(p_id)
-);
+CREATE TABLE `products` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `price` varchar(100) NOT NULL,
+    `image` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8mb4;
