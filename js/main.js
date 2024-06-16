fd28da2a63a27acfcd6d1fa2ddea6c65c1afddce
JazzMedo <84742298+Jazzmedo@users.noreply.github.com>
2024-06-16 07:28:33 +0300

    2.0

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const inputs = document.querySelectorAll(".input");
 
+console.log(inputs)
 function focusFunx() {
   let parent = this.parentNode.parentNode;
   parent.classList.add("focus");
 }
 inputs.forEach((input) => {
   input.addEventListener("focus", focusFunx);
+  console.log(input)
 });
